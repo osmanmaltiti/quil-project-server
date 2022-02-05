ba87c33f41f1b69f9c2597d167b58abaa6dea65b
osmanmaltiti <terabyte7778@gmail.com>
2022-02-05 11:21:40 -1200

    version 1.1

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const user = require('./routes/user-route');
 const port = process.env.PORT || 5000;
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
